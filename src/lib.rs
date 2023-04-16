9c73e664eaf2bb30666b3b7458047acaf60a23da
unknown <fhxmaster@gmail.com>
2023-04-17 05:43:50 +0700

    document check function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use chrono::Datelike;
 
+/// The function to check if the current day is Tuesday.
 pub fn check() -> bool {
     return matches!(chrono::offset::Local::now().date_naive().weekday(), chrono::Weekday::Tue);
 }
